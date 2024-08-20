f8ce1a5d9c55dbc2cd6edf8b5f691b185301ef7c
jsdaniell <jose.daniell@outlook.com>
2024-08-19 21:36:27 -0300

    uploaded code

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ const http = require('http');
 
 const PORT = 3000;
 
+let cars = [
+    { id: 1, brand: 'BMW', model: 'X5', cilinders: 6 },
+    { id: 2, brand: 'Audi', model: 'Q7', cilinders: 4 },
+    { id: 3, brand: 'Mercedes', model: 'GLE', cilinders: 6 },
+]
+
 const requestHandler = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
     const url = req.url.split("?")[0];
 
-    if (url === '/api' && req.method === 'GET') {
-        const cars = [
-            { id: 1, brand: 'BMW', model: 'X5' },
-            { id: 2, brand: 'Audi', model: 'Q7' },
-            { id: 3, brand: 'Mercedes', model: 'GLE' }
-        ]
-
-        let idToFind = 1
+    if (url === '/cars' && req.method === 'GET') {
+        let response;
 
         if(Boolean(req.url.split("?")[1])) {
-            const idParam =req.url.split("?")[1];
+            const idParam = req.url.split("?")[1];
 
             idToFind = Number(idParam.split("=")[1]);
+
+            response = cars.find(car => car.id === idToFind);
+        } else {
+            response = cars;
         }
-
-        const response = cars.find(car => car.id === idToFind);
-
+        
         res.writeHead(200);
         res.end(JSON.stringify(response));
-    } else if (req.url === "/api" && req.method === "POST") {
+    } else if (req.url === "/cars" && req.method === "POST") {
         let body = '';
 
         req.on('data', chunk => {
@@ -36,14 +38,23 @@ const requestHandler = (req, res) => {
         req.on('end', () => {
             const data = JSON.parse(body);
 
-            const response = {
-                message: 'Data received successfully',
-                data: data,
-                timestamp: new Date().toISOString()
-            };
+            if(!data?.brand || !data?.model || !data?.cilinders) {
+                res.writeHead(400);
+                res.end(JSON.stringify({ message: 'Bad request' }));
+                return;
+            }
 
-            res.writeHead(200);
-            res.end(JSON.stringify(response));
+            const newCar = {
+                id: cars.length + 1,
+                brand: data.brand,
+                model: data.model,
+                cilinders: data.cilinders
+            }
+
+            cars.push(newCar);
+
+            res.writeHead(201);
+            res.end(JSON.stringify(newCar));
         })
     } else {
         res.writeHead(404);
